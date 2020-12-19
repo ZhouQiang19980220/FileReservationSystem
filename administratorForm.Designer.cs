7c3ce461796fd1dac480475bca2e54f6045f0e2e
zhouqiang <zhou_qiang98@163.com>
2020-12-19 09:53:04 +0800

    修改管理员界面

---
+++ administratorForm.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace FileReservationSystem
+{
+    partial class administratorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button_lend = new System.Windows.Forms.Button();
+            this.button_exit = new System.Windows.Forms.Button();
+            this.button_query = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.Size = new System.Drawing.Size(788, 339);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button_lend
+            // 
+            this.button_lend.Location = new System.Drawing.Point(112, 356);
+            this.button_lend.Name = "button_lend";
+            this.button_lend.Size = new System.Drawing.Size(75, 33);
+            this.button_lend.TabIndex = 1;
+            this.button_lend.Text = "确认借出";
+            this.button_lend.UseVisualStyleBackColor = true;
+            // 
+            // button_exit
+            // 
+            this.button_exit.Location = new System.Drawing.Point(542, 356);
+            this.button_exit.Name = "button_exit";
+            this.button_exit.Size = new System.Drawing.Size(75, 33);
+            this.button_exit.TabIndex = 2;
+            this.button_exit.Text = "退出";
+            this.button_exit.UseVisualStyleBackColor = true;
+            // 
+            // button_query
+            // 
+            this.button_query.Location = new System.Drawing.Point(330, 356);
+            this.button_query.Name = "button_query";
+            this.button_query.Size = new System.Drawing.Size(75, 33);
+            this.button_query.TabIndex = 3;
+            this.button_query.Text = "查询预约记录";
+            this.button_query.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(460, 392);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(8, 8);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // administratorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button_query);
+            this.Controls.Add(this.button_exit);
+            this.Controls.Add(this.button_lend);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "administratorForm";
+            this.Text = "管理员界面";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button_lend;
+        private System.Windows.Forms.Button button_exit;
+        private System.Windows.Forms.Button button_query;
+        private System.Windows.Forms.Button button1;
+    }
+}
